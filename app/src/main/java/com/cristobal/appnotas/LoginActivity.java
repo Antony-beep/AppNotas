a7c0f75f611e57bc5e2d0d82f01428dc57a14a61
unknown <antonio.cristobal@tecsup.edu.pe>
2019-11-06 17:22:22 -0500

    Initial commit

---
+++ app/src/main/java/com/cristobal/appnotas/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.cristobal.appnotas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btnRegistro;
+    private Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnRegistro = findViewById(R.id.btn_registro);
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegistroApp.class));
+                finish();
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,NotasActivity.class));
+                finish();
+            }
+        });
+    }
+}
